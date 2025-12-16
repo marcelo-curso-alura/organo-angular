408a23a4e37d7d29dbdc72d7c795b4030320cdee
marcelo-curso-alura <marcelo.curso.alura@gmail.com>
2025-12-16 17:07:02 -0300

    CRUD Completo

--- src/app/componentes/formulario/formulario.component.ts
+++ src/app/componentes/formulario/formulario.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, input, OnInit, output, SimpleChanges } from '@angular/core';
+import { Component, input, OnChanges, OnInit, output, SimpleChanges } from '@angular/core';
 import { FormGroup, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
@@ -10,6 +10,7 @@ import { BotaoComponent } from '../botao/botao.component';
 import { DivisorComponent } from '../divisor/divisor.component';
 import { TituloComponent } from '../titulo/titulo.component';
 import { SubtituloComponent } from '../subtitulo/subtitulo.component';
+import { LivroService } from '../../services/livro.service';
 
 @Component({
   selector: 'app-formulario',
@@ -27,22 +28,30 @@ import { SubtituloComponent } from '../subtitulo/subtitulo.component';
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent implements OnInit {
+export class FormularioComponent implements OnInit, OnChanges {
+
+  livro = input<Livro | null>(null); 
+  submitForm = output<Livro>();  
   livroFormulario!: FormGroup;
-  generos: GeneroLiterario[] = [
-    { id: 'romance', value: 'Romance' },
-    { id: 'misterio', value: 'Mistério' },
-    { id: 'fantasia', value: 'Fantasia' },
-    { id: 'ficcao-cientifica', value: 'Ficção Científica' },
-    { id: 'tecnicos', value: 'Técnicos' }
-];
+  generos: GeneroLiterario[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
+    private livroService: LivroService
   ) { }
 
   ngOnInit() {
+    this.generos = this.livroService.generos;
     this.inicializarlivroFormulario();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['livro'] && this.livro()) {
+      this.livroFormulario.patchValue({
+        ...this.livro(),
+        genero: this.livro()?.genero?.id || ""
+      })
+    }
   }
 
   inicializarlivroFormulario() {
@@ -54,5 +63,14 @@ export class FormularioComponent implements OnInit {
       genero: [''],
       imagem: ['']
     })
-  };
+  }
+
+  emitirLivroAtualizado(){
+    const livroAtualizado: Livro = {
+      ...this.livroFormulario.value,
+      genero: this.generos.find(g => g.id === this.livroFormulario.value.genero)
+    }
+    this.submitForm.emit(livroAtualizado);
+  }
+
 }
